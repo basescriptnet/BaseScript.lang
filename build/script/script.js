851e5a620a302bd82306fe9e8e061cdb6287a96c
basescriptnet <basescriptnet@gmail.com>
2021-12-20 02:15:32 -0800

    Major file structure change. Src, lib, build.

---
+++ build/script/script.js
@@ -0,0 +1,367 @@
+if (!globalThis) {
+    globalThis = window || global || this || {};
+}
+try {
+    globalThis.require = require;
+} catch (err) {
+    globalThis.require = () => undefined;
+}
+//(async function () {
+// console.log(requirejs);
+globalThis.BS = {
+    Node(tagName, id, className, attributes, children) {
+        let el = document.createElement(tagName);
+        id && (el.id = id);
+        className && (el.className = className);
+        // attributes
+        if (children) {
+            el.append(children);
+        }
+        if (typeof attributes == 'object') {
+            for (let i in attributes) {
+                el.setAttribute(i, attributes[i]);
+            }
+        }
+        return el;
+    },
+    DOMtoJSON(node) {
+        // credit to sumn2u
+        node = node || this;
+        let obj = {
+            nodeType: node.nodeType
+        };
+        if (node.tagName) {
+            obj.tagName = node.tagName.toLowerCase();
+        } else
+        if (node.nodeName) {
+            obj.nodeName = node.nodeName;
+        }
+        if (node.nodeValue) {
+            obj.nodeValue = node.nodeValue;
+        }
+        let attrs = node.attributes;
+        let childNodes = node.childNodes;
+        let length;
+        let arr;
+        if (attrs) {
+            length = attrs.length;
+            arr = obj.attributes = new Array(length);
+            for (let i = 0; i < length; i++) {
+                const attr = attrs[i];
+                arr[i] = [attr.nodeName, attr.nodeValue];
+            }
+        }
+        if (childNodes) {
+            length = childNodes.length;
+            arr = obj.childNodes = new Array(length);
+            for (let i = 0; i < length; i++) {
+                arr[i] = this.DOMtoJSON(childNodes[i]);
+            }
+        }
+        return obj;
+    },
+    ast: (function() {
+        let r = globalThis.require('./index.js');
+        if (r) return r;
+        return function() {
+            console.warn('Ast is not supported yet in browsers.')
+        }
+    })(),
+    parse: (function() {
+        let r = globalThis.require('./ast_to_js.js');
+        if (r) return r;
+        return function() {
+            console.warn('@eval is not supported yet in browsers.')
+        }
+    })(),
+    fs: (function() {
+        let r = globalThis.require('fs');
+        if (r) return r;
+        return function() {
+            console.warn('@import is not supported yet in browsers.')
+        };
+    })(),
+    types: {
+        Array(value) {
+            return Array.isArray(value);
+        },
+        Null(value) {
+            return value === null;
+        },
+        Undefined(value) {
+            return value === void 0;
+        },
+        Int(value) {
+            return parseInt(value) === value && !Number.isNaN(value);
+        },
+        Float(value) {
+            return typeof value === 'number' && !Number.isNaN(value);
+        },
+        Number(value) {
+            return typeof value === 'number' && !Number.isNaN(value);
+        },
+        NaN(value) {
+            return Number.isNaN(value);
+        },
+        String(value) {
+            return typeof value === 'string';
+        },
+        Function(value) {
+            return typeof value === 'function';
+        },
+        Symbol(value) {
+            return typeof value === 'symbol';
+        },
+        Boolean(value) {
+            return typeof value === 'boolean';
+        },
+        HTML(value) {
+            return value instanceof Element;
+        },
+        Object(value) {
+            return typeof value === 'object' && value !== null;
+        },
+    },
+    convert(value, type) {
+        let t = this.getType(value)
+        if (t) t = t.toLowerCase();
+        let tmp = null;
+        if (typeof type !== 'object' && t == type.toLowerCase()) return value;
+        let gt = function(value, type) {
+            switch (type) {
+                case 'Number':
+                case 'Float':
+                    tmp = parseFloat(value)
+                    if (isNaN(tmp)) return 0;
+                    return tmp;
+                case 'Int':
+                    return value | 0;
+                case 'String':
+                    if (t == 'html') return JSON.stringify(this.DOMtoJSON(value));
+                    if (t == 'array') return value.join('');
+                    if (t == 'object') return JSON.stringify(value);
+                    if (t == 'boolean') return !!value;
+                    if (t == 'null') return 'null';
+                    if (t == 'undefined') return 'undefined';
+                    else return value.toString();
+                case 'Array':
+                    if (t == 'html') return Object.values(this.DOMtoJSON(value));
+                    if (t == 'object') return Object.values(value);
+                    if (t == 'string') {
+                        try {
+                            let z = JSON.parse(t)
+                            let v = this.getType(z) == 'array'
+                            if (v) return z;
+                            throw 'err';
+                        } catch (err) {}
+                        return value.split('');
+                    }
+                    if (t == 'number' || t == 'int' || t == 'float') {
+                        return ('' + value).split('')
+                    } else return [value];
+                case 'JSON':
+                    if (t == 'html') return this.DOMtoJSON(value);
+                    if (t == 'object' || t == 'string' || t == 'array' || t == 'null' || t == 'number')
+                        return value;
+                    throw new TypeError(`Cannot convert ${t} to ${type}`);
+                case 'Object':
+                    return Object(value);
+                case 'Boolean':
+                    return Boolean(value);
+                case 'List':
+                    if (t == 'object' || t == 'array' || t == 'html') {
+                        let ul = document.createElement('ul');
+                        for (let i in value) {
+                            let li = document.createElement('li');
+                            if (t == 'object') {
+                                let span = document.createElement('span');
+                                span.className = 'convert_key';
+                                span.innerText = i;
+                                li.append(span);
+                                li.appendChild(document.createTextNode(': '))
+                            }
+                            let span = document.createElement('span');
+                            span.className = 'convert_value';
+                            if (this.getType(value[i]) == 'string') {
+                                span.className += ' string';
+                                span.innerText = `"${value[i]}"`;
+                            } else if (this.getType(value[i]) == 'number') {
+                                span.className += ' number';
+                                span.innerText = `${value[i]}`;
+                            } else if (this.getType(value[i]) == 'array') {
+                                span.className += ' array';
+                                span.innerText = JSON.stringify(value[i], null, 2);
+                            } else span.innerText = `${value[i]}`;
+                            li.append(span);
+                            ul.append(li);
+                        }
+                        return ul;
+                    }
+                    return document.createElement('ul');
+            }
+        }
+        if (typeof type != 'string') {
+            let r = value;
+            // debugger
+            console.log(type.length)
+            for (let i = 0; i < type.length; i++) {
+                r = gt(r, type[i]);
+                console.log({
+                    r,
+                    type: type[i]
+                })
+                if (i != length - 1) {
+                    if (r.map) {
+                        r = r.map(j => {
+                            console.log({
+                                each: this.convert(j, type[i + 1]),
+                                type: type[i + 1]
+                            })
+                            return this.convert(j, type[i + 1])
+                        });
+                    }
+                }
+                console.log(r)
+            }
+        }
+        // console.log('h', gt(value,type))
+        return gt(value, type);
+    },
+    getType(value) {
+        for (let i in this.types) {
+            try {
+                if (this.types[i](value)) return i;
+            } catch (err) {
+                continue
+            }
+        }
+    },
+    storage: [],
+    // require('./ast_to_js.js')// || function () {console.warn('@eval is not supported yet in browsers.')}
+};
+try {
+    Element;
+} catch {
+    globalThis.Element = {
+        prototype: {}
+    };
+}
+Element.prototype._append = Element.prototype.append;
+Element.prototype.append = function(children) {
+    if (Array.isArray(children)) {
+        for (let i = 0, l = children.length; i < l; i++) {
+            this._append(children[i]);
+        }
+    } else {
+        this._append(children);
+    }
+    return this;
+}
+Element.prototype.json = function() {
+    return globalThis.BS.DOMtoJSON(this);
+}
+Element.prototype.text = function(content) {
+    if (content === void 0) {
+        return this.textContent;
+    }
+    this.textContent = content;
+    return this;
+}
+Element.prototype.css = function(obj) {
+    for (let i in obj) {
+        this.style[i] = obj[i];
+    }
+    return this;
+}
+Element.prototype.events = function(obj) {
+    for (let i in obj) {
+        this.addEventListener(i, obj[i]);
+    }
+    return this;
+}
+Array.prototype.last = function() {
+    return this[this.length - 1];
+}
+
+function list(amount, callback) {
+    let array = [];
+    for (let i = 0; i < amount; i++) {
+        if (typeof callback === 'function') {
+            console.log(i);
+            array.push(callback(i));
+        } else {
+            array.push(callback);
+        }
+    }
+    return array;
+}
+
+function range(n) {
+    let i = 0 // start
+    return { // iterator protocol
+        [Symbol.iterator]: () => { // @@iterator
+            return { // object with the next function
+                next() {
+                    while (i !== n) {
+                        let temp = i
+                        i++
+                        return {
+                            value: temp,
+                            done: false
+                        }
+                    }
+                    return {
+                        done: true
+                    }
+                }
+            }
+        }
+    }
+}
+const PI = 3.141592653589793,
+    E = 2.718281828459045,
+    log = (...args) => console.log(...args),
+    sqrt = Math.sqrt.bind(Math),
+    Time = {
+        get now() {
+            return Date.now()
+        },
+        get ms() {
+            return new Date().getMilliseconds()
+        },
+        get seconds() {
+            return new Date().getSeconds()
+        },
+        get minutes() {
+            return new Date().getMinutes()
+        },
+        get hours() {
+            return new Date().getHours()
+        },
+        get day() {
+            return new Date().getDay()
+        },
+        get month() {
+            return new Date().getMonth()
+        },
+        get year() {
+            return new Date().getFullYear()
+        },
+    };
+
+// your code below this line!
+
+function g(a, b) {
+    if (!globalThis.BS.types["Int"](a)) throw new TypeError("Argument \"a\" is not type of Int at line 1, col 12.");
+    if (!globalThis.BS.types["Int"](b)) throw new TypeError("Argument \"b\" is not type of Int at line 1, col 12.");
+    return sqrt(a ** 2 + b ** 2);
+};
+console.log(g(10, 20));
+globalThis.BS.Node("ul", "n", "hello world f", null);
+let a = globalThis.BS.Node("ul", null, null, {
+    id: "hello",
+    n: "10",
+    z: "20",
+}, [globalThis.BS.Node("li", null, null, null, ["Hello"])]);
+document.body.append(a);
+//})();
--- index.js
+++ index.js
@@ -1,32 +1,84 @@
+
+const beautify = require('js-beautify').js;
 const fs = require('fs');
-const parser = require('./parser.js');
-function readFile (fileName) {
-    let content = null;
-    // try {
-        content = fs.readFileSync(fileName, { encoding: "utf8", flag: "r" });
-    // } catch (err) {
-    //     throw err;
-    // }
-    return content;
-}
-module.exports = (string) => {
-    // if string is provided
-    // console.log(1)
-    if (!/\.bs$/.test(string)) { 
-        try {
-            let result = parser(string+'\n');
-            return result;
-        } catch (err) {
-            console.log(err)
+const BS = require('./lib/compiler');
+// const watch = require('./src/watch/watch');
+// watch.call(this, './test');
+let lastChange = 0;
+let lastFile = '';
+let writeFile = (path, fileName, content) => {
+    let date = Date.now();
+    let ast = '';
+    try {
+        ast = JSON.stringify(content, null, 4)//.replace(/^\\#([A-Za-z0-9]{3}|[A-Za-z0-9]{6})$/g, '#')
+        // console.clear();
+        console.log('[File System]: Writing to: '+fileName+'.js')
+        // writing as ast file
+        // fs.writeFileSync(
+        //     `${fileName}.ast`,
+        //     ast
+        // );
+        // ----- new
+        let parse = require('./lib/compiler/ast_to_js')
+        var content = parse(JSON.parse(ast));
+        let built_in = fs.readFileSync('./lib/compiler/built_in.js', { recursive: true , encoding: 'utf-8'});
+        // fs.rmdirSync('./build', { recursive: true });
+        // fs.mkdirSync('./build/');
+        let p = fileName.split('\\').slice(0, -1);
+        if (!fs.existsSync(p.join('/'))) {
+            fs.mkdirSync('./build/'+p.join('/'), {recursive: true})
+        }
+
+        fs.writeFileSync(
+            `./build/${fileName.replace('\\', '/')}.js`,
+            beautify(`
+            if (!globalThis) {
+                globalThis = window || global || this || {};
+            }
+            try {
+                globalThis.require = require;
+            } catch (err) {
+                globalThis.require = () => undefined;
+            }
+            //(async function () {
+                ${built_in}
+                ${content}
+            //})();\n`)
+            // beautify(`
+            //     ${content}
+            // \n`)
+        );
+        console.log('Compiled in ' + (Date.now() - date) + 'ms');
+        // ----- new end
+        return ast;
+    } catch (err) {
+        console.log(err);
+        return void console.error('[Error]: Can\'t compile. Unexpected input.')
+    }
+};
+
+fs.watch("./src/", { recursive: true }, function(event, path) {
+    if (event != 'change') return;
+    if (!/\.bs$/i.test(path)) return;
+    if (path == lastFile && lastChange + 10 > Date.now()) return;
+    lastChange = Date.now();
+    lastFile = path;
+
+    console.log(path, 'is', event);
+    console.clear()
+    let fileName = path.substr(0, path.length-('.bs'.length));
+    try {
+        let content = BS(`./src/${path}`);
+        if (content === void 0) {
+            console.log(']')
             return;
         }
-    }
-    // if file path is provided
-    let file = readFile(string);
-    try {
-        let result = parser(file+'\n');
-        return result;
+        let result = writeFile(path, fileName, content);
+        // result !== void 0 && console.log(`[Output]: ${result}`);
     } catch (err) {
-        console.log(err)
+        console.warn(new Error('Can\'t compile. Unexpected input.'));
+        console.warn(new Error(err.message));
     }
-}
+    console.log(']')
+});
+// console.log(parse('\\a = 10'));
